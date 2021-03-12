9273f843d60a2fb181932a3e7d33191ee310ef7c
sskoredin <sergey@skoredin.pro>
2021-03-12 13:15:57 +0300

    timezone

--- main.go
+++ main.go
@@ -3,17 +3,29 @@ package main
 import (
 	"github.com/sskoredin/iiko_report/app"
 	logger "github.com/sskoredin/telegram_client"
+	"log"
+	"time"
+	_ "time/tzdata"
 )
 
-func main() {
-	runApp()
+func init() {
+	loc, err := time.LoadLocation("Europe/Moscow")
+	if err != nil {
+		log.Fatal(err)
+	}
+	time.Local = loc // -> this is setting the global timezone
 }
 
-func runApp() {
-	a := app.New()
-	l := logger.New("main")
-	if err := a.Run(); err != nil {
-		l.Error(err.Error())
-		return
+func main() {
+	log := logger.New("main")
+	log.Infof("Start iiko report at %v", time.Now().String())
+
+	if err := runApp(); err != nil {
+		log.Fatal(err)
 	}
+}
+
+func runApp() error {
+	a := app.New()
+	return a.Run()
 }
